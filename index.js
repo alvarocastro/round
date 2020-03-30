9bc558d2a7074ce4dff9736aeb3e2f4bec3ba224
Alvaro Castro <moltar89@gmail.com>
2020-03-30 18:52:24 -0300

    feat: add validations

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const round = function (number, decimals = 2) {
+	if (typeof number !== 'number') {
+		throw new TypeError('Must pass a number');
+	}
+
+	if (!Number.isInteger(decimals)) {
+		throw new TypeError('The amount of decimals must be an integer number');
+	}
+
+	if (decimals < 0) {
+		throw new TypeError('The amount of decimals can’t be negative');
+	}
+
 	const p = 10 ** decimals;
 	const fpFix = Number.EPSILON * (number < 0 ? -1 : 1);
 
